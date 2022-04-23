6515aba6f04093d90b5c00abe6ca852e2dca041d
juds-lee <judyjungmin.lee@gmail.com>
2022-04-23 19:04:29 -0400

    added styling and functionality

--- script/script.js
+++ script/script.js
@@ -4,6 +4,27 @@ app.learnMore = document.querySelector(".learnMore");
 app.modal = document.querySelector(".modalBG");
 app.typed = document.querySelector('.typed');
 app.stickyHeader = document.querySelector("header");
+app.sections = document.querySelectorAll("header, section, footer");
+app.navItem = document.querySelectorAll(".navItem");
+
+window.addEventListener("scroll", () => {
+ let currentSection = ""; 
+ app.sections.forEach( section => {
+   const sectionTop = section.offsetTop
+   const sectionHeight = section.clientHeight;
+   if (pageYOffset >= sectionTop/3) {
+     currentSection = section.getAttribute("id"); 
+   }
+   console.log(currentSection)
+ })
+ app.navItem.forEach(li => {
+  li.classList.remove("scrollActive");
+  if (li.classList.contains(currentSection)){
+    li.classList.add("scrollActive");
+  }
+ })
+})
+
 
 
 app.typewriter = new Typewriter(app.typed, {
@@ -25,3 +46,4 @@ app.closeButton.addEventListener("click", () => {
 window.addEventListener("scroll",function() {
   app.stickyHeader.classList.toggle("sticky", window.scrollY > 0);
 })
+
